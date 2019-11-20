03a7ccefd80fa7cf33d7630ae0eb4ce3760a7548
Alrifki Fajriansyah <alrifki1224@gmail.com>
2019-11-20 14:14:03 +0700

    product tour

---
+++ resources/views/menu.blade.php
@@ -0,0 +1,352 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
+        integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css?family=Forum|Knewave&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css"
+        integrity="sha384-fnmOCqbTlWIlj8LyTjo7mOUStjsKC4pOpQbqyi7RrhN7udi9RwhKkMHpvLbHG9Sr" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title>pergi umroh</title>
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-primary bg-primary fixed-top">
+        <div class="container">
+            <a class="navbar-brand text-white" href="#" style="font-family: 'Knewave', cursive;">PergiUmroh Blog</a>
+            <form class="form-inline my-2 my-lg-0">
+                <input class="form-control mr-sm-2 outline-success ce" type="search" placeholder="Search"
+                    aria-label="Search">
+            </form>
+        </div>
+    </nav>
+    <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+        <div class="carousel-inner">
+            <div class="carousel-item active">
+                <div class="content1">
+                    <div class="container ">
+                        <div class="row mt-5">
+                            <div class="col-sm-6 ">
+                                <img src="img/masjid.jpeg" class="img-fluid" alt="Responsive image">
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="text text-light  outline-dark">
+                                    <h4>Indahnya Masjid Al Farooq Dubai, Terinspirasi Keadilan Khalifah Umar</h4>
+                                    <p>Jika di Dubai kamu cuma melihat berbagai gedung tinggi dan pusat perbelanjaan,
+                                        wah berarti
+                                        jalan-jalan kamu kurang jauh nih. Diantara gemerlap gedung megah dan mewah yang
+                                        ada di
+                                        Dubai,
+                                        ada juga oase yang hebat dan menentramkan yaitu Masjid Al Farooq. Dubai ternyata
+                                        menyimpan
+                                        berbagai masjid-masjid indah yang salah satunya adalah Masjid Al Farooq Omar ibn
+                                        Khattab
+                                        ini,
+                                        yang juga memiliki status masjid terbesar di Dubai.</p>
+
+                                    <a
+                                        href="https://pergiumroh.com/blog/keindahan-masjid-al-farooq-dubai-terinspirasi-umar-bin-khattab"><button
+                                            type="button" class="btn btn-primary mb-4">Read More</button></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="content1">
+                    <div class="container ">
+                        <div class="row mt-5">
+                            <div class="col-sm-6 ">
+                                <img src="img/masjid2.jpeg" class="img-fluid" alt="Responsive image">
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="text text-light  outline-dark">
+                                    <h4>Ingin Merasakan Haji Bersama Nabi? Begini Caranya!</h4>
+                                    <p>Bulan Ramadhan identik dengan bulan yang penuh dengan banyak keistimewaan bagi
+                                        umat islam. Benar saja, saking istimewanya bulan Ramadhan adalah bulan
+                                        terfavorit untuk melaksanakan ibadah umroh ke tanah suci. Makanya jangan heran
+                                        jika Anda akan melihat banyak promo umroh saat bulan Ramadhan tiba. Karena
+                                        muslim di seluruh penjuru dunia yang belum berkesempatan haji akan merencanakan
+                                        untuk umroh pada bulan yang penuh berkah ini.</p>
+
+                                    <a href="https://pergiumroh.com/blog/cara-untuk-bisa-merasakan-haji-bersama-nabi"><button
+                                            type="button" class="btn btn-primary mb-4">Read More</button></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="carousel-item">
+                <div class="content1">
+                    <div class="container ">
+                        <div class="row mt-5">
+                            <div class="col-sm-6 ">
+                                <img src="img/masjid3.jpeg" class="img-fluid" alt="Responsive image">
+                            </div>
+                            <div class="col-sm-6">
+                                <div class="text text-light  outline-dark">
+                                    <h4>Sholat di Hijr Ismail Setara dengan Sholat dalam Ka'bah</h4>
+                                    <p>Sungguh tak ternilai rasa syukurnya kalau kita bisa masuk dan sholat di dalam
+                                        Ka’bah. Namun, tentu tidak sembarang orang yang bisa masuk ke dalam ka’bah.
+                                        Ternyata memang hanya segelintir orang yang bisa mendapat kehormatan ini,
+                                        seperti raja dan presiden-presiden lain dari berbagai negara yang tentunya
+                                        beragama Islam. Presiden-presiden tersebut yaitu Presiden Republik Tajikistan
+                                        (Emomali Rahmon), Presiden Republik Azerbaijan (Ilham Aliyev), Perdana Menteri
+                                        Malaysia (Datuk Seri Najib Razak), dan akhir-akhir ini yaitu Pak Joko Widodo
+                                        yaitu Presiden RI kita yang akrab disapa sebagai Jokowi.</p>
+
+                                    <a
+                                        href="https://pergiumroh.com/blog/sholat-di-hijr-ismail-setara-sholat-dalam-kabah"><button
+                                            type="button" class="btn btn-primary mb-4">Read More</button></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+        </a>
+        <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+        </a>
+    </div>
+
+    <div class="content2 ">
+        <div class="container">
+            <p class="text mx-auto text-primary mt-5" style="font-family: 'Knewave', cursive;">Artikel Seputar Umroh</p>
+            <div class="row mt-5">
+                <div class="col-sm-6">
+                    <a href="https://pergiumroh.com/blog/boleh-gak-selfie-di-masjidil-haram-dan-masjid-nabawi">
+                        <div class="card mb-3 mt-5">
+                            <div class="row ">
+                                <div class="col-sm-5">
+                                    <img src="img/conten1.jpeg" class="card-img pt-sm-3 ml-sm-1" alt="Responsive image">
+                                </div>
+                                <div class="col-md-7">
+                                    <div class="card-body">
+                                        <p class="text-danger font-weight-bolder">BERITA</p>
+                                        <h5 class="card-text">Selfie di Masjidil Haram dan Masjid Nabawi itu Boleh Gak
+                                            Sih?</h5>
+                                        <p class="card-text"><small class="text-muted">13 Agustus 2019</small></p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-6">
+                    <a href="https://pergiumroh.com/blog/keistimewaan-dua-kali-sholat-jumat-saat-umroh">
+                        <div class="card mb-3 mt-5">
+                            <div class="row ">
+                                <div class="col-sm-5">
+                                    <img src="img/conten2.jpeg" class="card-img pt-sm-3 ml-sm-1" alt="Responsive image">
+                                </div>
+                                <div class="col-md-7">
+                                    <div class="card-body">
+                                        <p class="text-danger font-weight-bolder">BERITA</p>
+                                        <h5 class="card-text">Kenapa Jemaah Lebih Suka 2x Sholat Jumat Saat Umroh?</h5>
+                                        <p class="card-text"><small class="text-muted">9 September 2019</small></p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-6">
+                    <a href="https://pergiumroh.com/blog/rangkaian-ibadah-haji-proses-dan-tempat-wajib">
+                        <div class="card mb-3 mt-5">
+                            <div class="row ">
+                                <div class="col-sm-5">
+                                    <img src="img/umroh.jpeg" class="card-img pt-sm-3 ml-sm-1" alt="Responsive image">
+                                </div>
+                                <div class="col-md-7">
+                                    <div class="card-body">
+                                        <p class="text-danger font-weight-bolder">BERITA</p>
+                                        <h5 class="card-text">Kenali Rangkaian Ibadah Haji Lebih Dalam: Proses dan
+                                            Tempat Wajib</h5>
+                                        <p class="card-text"><small class="text-muted">26 Agustus 2019</small></p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-sm-6">
+                    <a href="https://pergiumroh.com/blog/amalan-sederhana-yang-dapat-dilakukan-saat-umroh">
+                        <div class="card mb-3 mt-5">
+                            <div class="row ">
+                                <div class="col-sm-5">
+                                    <img src="img/conten3.jpeg" class="card-img pt-sm-3 ml-sm-1" alt="Responsive image">
+                                </div>
+                                <div class="col-md-7">
+                                    <div class="card-body">
+                                        <p class="text-danger font-weight-bolder">BERITA</p>
+                                        <h5 class="card-text">Amalan Sederhana yang Dapat Dilakukan Saat Umroh</h5>
+                                        <p class="card-text"><small class="text-muted">18 Juli 2019</small></p>
+
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="contetn3">
+        <div class="container">
+            <p class="text mx-auto text-primary mt-3" style="font-family: 'Knewave', cursive;">Video</p>
+            <div class="row mt-5 mb-5">
+                <div class="col-sm-8">
+                    <style>
+                        .embed-container {
+                            position: relative;
+                            padding-bottom: 56.25%;
+                            height: 0;
+                            overflow: hidden;
+                            max-width: 100%;
+                        }
+
+                        .embed-container iframe,
+                        .embed-container object,
+                        .embed-container embed {
+                            position: absolute;
+                            top: 0;
+                            left: 0;
+                            width: 100%;
+                            height: 100%;
+                        }
+                    </style>
+                    <div class='embed-container'><iframe src='https://www.youtube.com/embed/lcG0xQrIS4A' frameborder='0'
+                            allowfullscreen></iframe></div>
+                </div>
+                <div class="col-sm-4">
+                    <h3><b>Kini pergi umroh ngga pake ribet!!!</b></h3>
+                    <p>di era digital, pergi umroh bukan merupakan hal yang sulit saat ini.
+                        banyak pilihan paket yang super ekonomis namun tetap mewah.
+                        tidak hanya pergi umroh, namun kalian bisa ketempat-tempat destinasi yang keren loh, misalnya ke
+                        turki, dubai, dan lain-lainnya.
+                        <h5>Yuk kita pergi umroh bareng Pergiumroh.com</h5>
+                    </p>
+                    <a href="https://pergiumroh.com/"><button type="button" class="btn btn-primary">Pergiumroh.com
+                        </button></a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="content4">
+        <div class="container">
+            <p class="text mx-auto text-primary mt-5" style="font-family: 'Knewave', cursive;">Artikel selengkapnya</p>
+            <div class="row mt-5">
+                <div class="col-md-3">
+                    <a href="https://pergiumroh.com/blog/manasik-haji-dan-manfaatnya-bagi-calon-jemaah-haji">
+                        <div class="card mb-3 mt-5">
+                            <img src="img/content4.jpeg" class="card-img-top" alt="Responsive image">
+                            <div class="card-body">
+                                <p class="card-text">Manasik Haji dan Manfaatnya Bagi Calon Jemaah Haji</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a href="https://pergiumroh.com/blog/mengenal-adab-masuk-masjid-sesuai-sunnah-nabi">
+                        <div class="card mb-3 mt-5">
+                            <img src="img/conten5.jpeg" class="card-img-top" alt="Responsive image">
+                            <div class="card-body">
+                                <p class="card-text">Mengenal Adab Masuk Masjid Sesuai Sunnah Nabi</p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a href="https://pergiumroh.com/blog/tata-cara-manasik-umroh-dan-prosesnya">
+                        <div class="card mb-3 mt-5">
+                            <img src="img/conten6.jpeg" class="card-img-top" alt="Responsive image">
+                            <div class="card-body">
+                                <p class="card-text">Belajar Tentang Umroh? Ini Nih Manasik Umroh dan Rangkaian
+                                    Prosesinya
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+                <div class="col-md-3">
+                    <a href="https://pergiumroh.com/blog/tata-cara-manasik-umroh-dan-prosesnya">
+                        <div class="card mb-3 mt-5">
+                            <img src="img/conten6.jpeg" class="card-img-top" alt="Responsive image">
+                            <div class="card-body">
+                                <p class="card-text">Belajar Tentang Umroh? Ini Nih Manasik Umroh dan Rangkaian
+                                    Prosesinya
+                                </p>
+                            </div>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="footer">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-3">
+                    <img src="img/logo.png" width="200" height="70" alt="Responsive image">
+                </div>
+                <div class="col-sm-5 pt-4">
+                    <a href="#">
+                        Pergiumroh
+                    </a>
+                    <a href="#" class="ml-3">
+                        Kilau Umroh
+                    </a>
+                    <a href="#" class="ml-3">
+                        Promo
+                    </a>
+                    <a href="#" class="ml-3">
+                        Paket Umroh
+                    </a>
+                    <a href="#" class="ml-3">
+                        Blog
+                    </a>
+                </div>
+                <div class="col-sm-4 pt-4 justify-content-end">
+                    <a href="#" class="ml-5">
+                        <i class="fab fa-facebook-f fa-lg text-primary"></i>
+                    </a>
+                    <a href="#" class="ml-4">
+                        <i class="fab fa-twitter fa-lg text-info"></i>
+                    </a>
+                    <a href="#" class="ml-4">
+                        <i class="fab fa-instagram fa-lg text-dark"></i>
+                    </a>
+                    <a href="#" class="ml-4 ">
+                        <i class="fab fa-youtube fa-lg text-danger"></i>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"
+        integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"
+        crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"
+        integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1"
+        crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js"
+        integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM"
+        crossorigin="anonymous"></script>
+</body>
+
+</html>
